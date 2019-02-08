96130662fa23ef2f22ac787478c18184237c1a5e
rajacsp <raja.csp@gmail.com>
2019-02-08 05:54:35 -0500

    DBWL write updated

---
+++ org/dwyl/db_util.py
@@ -0,0 +1,160 @@
+'''
+Created on Apr 20, 2017
+
+@author: raja
+
+source:
+    https://www.w3schools.com/python/python_mysql_insert.asp
+'''
+
+#!/usr/bin/python
+import base64
+import sys
+import codecs
+import MySQLdb
+
+def test():
+    print('tst 123')
+
+def get_db():
+    db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="test",         # your username
+                     passwd="test",  # your password
+                     db="test")        # name of the data base
+    
+    return db
+    
+def get_db_cursor():
+    db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="test",         # your username
+                     passwd="test",  # your password
+                     db="test")        # name of the data base    
+
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    return db.cursor()
+
+
+'''
+    Add City
+'''
+def add_dwyl(db, github_link, hit_code, hit_n_code):
+    
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor()
+    
+    #
+    sql = "INSERT INTO DWLY_HITS (GITHUB_LINK, HIT_CODE, HIT_N_CODE, ADDED_DATE) VALUES (%s, %s, %s, now())"
+    values = (github_link, hit_code, hit_n_code)
+
+    # Use all the SQL you like
+    cur.execute(sql, values)
+
+    db.commit()
+
+    print('Done : '+str(cur.rowcount)+" inserted") 
+    
+    
+'''
+    Update City
+'''    
+def update_city(db, name, state, country, id):
+    
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor()
+    
+    #
+    sql = "UPDATE CITY SET NAME = %s, STATE = %s, COUNTRY = %s WHERE ID = %s"
+    values = (name, state, country, id)
+
+    # Use all the SQL you like
+    cur.execute(sql, values)
+
+    db.commit()
+
+    print('Done : '+str(cur.rowcount)+" updated") 
+
+
+'''
+    Delete City
+'''    
+def delete_city(db, id):
+    
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor()
+    
+    #
+    sql = "DELETE FROM CITY WHERE ID = %s"
+    values = (id, )
+
+    # Use all the SQL you like
+    cur.execute(sql, values)
+
+    db.commit()
+    
+    if(cur.rowcount == 0):
+        raise Exception("Item Not Available to Delete")
+    
+    if(cur.rowcount > 1):
+        raise Exception("More than one item deleted")
+
+    print('Done : '+str(cur.rowcount)+" deleted")            
+
+
+'''
+    Read City
+'''
+def read_city(db):
+    
+    cur = db.cursor()
+    
+    # Use all the SQL you like
+    cur.execute("SELECT * FROM city")
+
+    # print all the first cell of all the rows
+    counter = 0;
+    for row in cur.fetchall():
+        try:
+            counter = counter + 1
+        
+            pid = str(row[0])
+            name = str(row[1])
+            state = str(row[2])
+            country = str(row[3])
+        
+            print(pid + " - " +  name + " - " + state + " - "+country)
+        except ValueError as error:
+            print('Error', format(error))
+
+    
+
+def main():
+    
+    
+    db = get_db()
+    
+    # C - add city
+    add_dwyl(db, 'Theni', 'TA', 'India')
+    
+    # R - read city
+    read_city(db)
+    
+    # U - update city
+    #update_city(db, 'Thennai', 'TA', 'India', 5)
+    
+    # D - delete city
+    try:
+        delete_city(db, 10)
+    except Exception as er:
+        print("Error : ", format(er))
+    
+    # close DB
+    db.close()
+    
+
+if __name__ == '__main__':
+    #main()
+    pass
---
+++ org/dwyl/dwyl_writer.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+author:
+    raja
+
+source: (if any)
+   https://stackoverflow.com/questions/3277503/in-python-how-do-i-read-a-file-line-by-line-into-a-list 
+'''
+
+import requests
+import db_util as dbu
+import MySQLdb
+
+def print_file(filename):
+
+    db = dbu.get_db()
+
+    with open(filename) as f:
+        content = f.readlines()
+        
+    counter = 0
+
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    for x in content:
+
+        counter = counter + 1
+
+        if(len(x.strip()) == 0):
+            continue
+
+        items = x.split(" ")
+
+        #if(counter > 5):
+        #    break
+
+        #print(len(items))
+
+        if(len(items) != 5):
+            continue
+
+        g_link = 'https://github.com' + items[2]       
+        #print(g_link)
+        hit_n_code = items[3].strip()
+        hit_code = items[4].strip()
+        
+
+        print('----------')
+        print(g_link)
+        print(hit_code)
+        print(hit_n_code)
+
+        # db, github_link, hit_code, hit_n_code
+        try:
+            dbu.add_dwyl(db, g_link, hit_code, hit_n_code)
+        except MySQLdb.IntegrityError:
+            print('Integrity error')
+        
+
+        #print(x)    
+    
+    print('\n\nDone!')
+
+def is_page_alive(url):
+    r = requests.get(url)
+    #print(r.status_code)
+
+    if(r.status_code == 200):
+        return True
+    else:
+        return False    
+            
+if __name__ == '__main__':    
+    print_file("public_projects_from_hits_dwyl.txt")
+    #dbu.test()
